c48a34d68dda7a3066de95ea47272974b5085d8b
ayushmancode672 <jayushman63@gmail.com>
2024-07-28 13:20:48 +0530

    complete

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert("Hello! I am an alert box!!");
+let circle = document.querySelector('.circle');
+let moveby = 10;
+
+window.addEventListener('load', () => {
+    circle.style.position = 'absolute';
+    circle.style.left = '0px';
+    circle.style.top = '0px';
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'ArrowLeft':
+            circle.style.left = parseInt(circle.style.left) - moveby + 'px';
+            break;
+        case 'ArrowRight':
+            circle.style.left = parseInt(circle.style.left) + moveby + 'px';
+            break;
+        case 'ArrowUp':
+            circle.style.top = parseInt(circle.style.top) - moveby + 'px';
+            break;
+        case 'ArrowDown':
+            circle.style.top = parseInt(circle.style.top) + moveby + 'px';
+            break;
+    }
+});
